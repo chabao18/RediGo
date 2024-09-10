3456712f4ea680791c1931f3381a1535e86193a3
Zhixiong Lu <zhixiong.lu@qq.com>
2024-09-10 22:48:59 +0800

    Handle server error

--- tcp/server.go
+++ tcp/server.go
@@ -5,6 +5,10 @@ import (
 	"RediGo/lib/logger"
 	"context"
 	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 type Config struct {
@@ -12,11 +16,19 @@ type Config struct {
 	Address string
 }
 
-func ListenAndServeWithSignal(
-	cfg *Config,
+func ListenAndServeWithSignal(cfg *Config,
 	handler tcp.Handler) error {
 
 	closeChan := make(chan struct{})
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		sig := <-sigChan
+		switch sig {
+		case syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			closeChan <- struct{}{}
+		}
+	}()
 	listener, err := net.Listen("tcp", cfg.Address)
 	if err != nil {
 		return err
@@ -26,12 +38,24 @@ func ListenAndServeWithSignal(
 	return nil
 }
 
-func ListenAndServe(
-	listener net.Listener,
-	handler tcp.Handler,
-	closeChan <-chan struct{}) {
+func ListenAndServe(listener net.Listener,
+	handler tcp.Handler, closeChan <-chan struct{}) {
 
+	go func() {
+		<-closeChan
+		logger.Info("shutting down")
+		_ = listener.Close()
+		_ = handler.Close()
+	}()
+
+	// close listener and handler
+	defer func() {
+		_ = listener.Close()
+		_ = handler.Close()
+	}()
 	ctx := context.Background()
+	// handle error
+	var waitDone sync.WaitGroup
 	for true {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -39,8 +63,13 @@ func ListenAndServe(
 		}
 
 		logger.Info("accepted link")
+		waitDone.Add(1)
 		go func() {
+			defer func() {
+				waitDone.Done()
+			}()
 			handler.Handle(ctx, conn)
 		}()
 	}
+	waitDone.Wait()
 }
