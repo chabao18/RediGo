8490ed0e3eb119e99aa240f2ff06407969da43c7
Zhixiong Lu <zhixiong.lu@qq.com>
2024-09-11 19:55:24 +0800

    Add comments

--- tcp/server.go
+++ tcp/server.go
@@ -11,16 +11,18 @@ import (
 	"syscall"
 )
 
+// Config holds the configuration parameters for TCP server.
 type Config struct {
-	// tcp params config
+	// Address is the TCP address the server listens on.
 	Address string
 }
 
-func ListenAndServeWithSignal(cfg *Config,
-	handler tcp.Handler) error {
-
+// ListenAndServeWithSignal starts a TCP server and listens for system signals to gracefully shut down.
+func ListenAndServeWithSignal(cfg *Config, handler tcp.Handler) error {
 	closeChan := make(chan struct{})
 	sigChan := make(chan os.Signal)
+
+	// Register signal notifications for graceful shutdown.
 	signal.Notify(sigChan, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
 	go func() {
 		sig := <-sigChan
@@ -29,47 +31,50 @@ func ListenAndServeWithSignal(cfg *Config,
 			closeChan <- struct{}{}
 		}
 	}()
+
+	// Start listening on the specified TCP address.
 	listener, err := net.Listen("tcp", cfg.Address)
 	if err != nil {
 		return err
 	}
-	logger.Info("start listen" + cfg.Address)
+	logger.Info("Start listening on " + cfg.Address)
+
+	// Start the server with the specified handler and close channel.
 	ListenAndServe(listener, handler, closeChan)
 	return nil
 }
 
-func ListenAndServe(listener net.Listener,
-	handler tcp.Handler, closeChan <-chan struct{}) {
-
+// ListenAndServe handles incoming TCP connections and shuts down gracefully on receiving a signal.
+func ListenAndServe(listener net.Listener, handler tcp.Handler, closeChan <-chan struct{}) {
 	go func() {
 		<-closeChan
-		logger.Info("shutting down")
+		logger.Info("Shutting down")
 		_ = listener.Close()
 		_ = handler.Close()
 	}()
 
-	// close listener and handler
+	// Ensure listener and handler are closed when function exits.
 	defer func() {
 		_ = listener.Close()
 		_ = handler.Close()
 	}()
+
 	ctx := context.Background()
-	// handle error
 	var waitDone sync.WaitGroup
-	for true {
+
+	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			break
 		}
 
-		logger.Info("accepted link")
+		logger.Info("Accepted connection")
 		waitDone.Add(1)
 		go func() {
-			defer func() {
-				waitDone.Done()
-			}()
+			defer waitDone.Done()
 			handler.Handle(ctx, conn)
 		}()
 	}
+
 	waitDone.Wait()
 }
